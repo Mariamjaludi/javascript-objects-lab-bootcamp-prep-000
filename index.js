a00b6b892a5b73d78fbb30b343d0735b1db9b178
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-14 19:30:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
+
+function updateObjectWithKeyAndValue(object, key, value){
+    return Object.assign({}, object, {[key: value]})
+  
+}
